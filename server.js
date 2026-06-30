5b37bf300f0aa2636146217c394ab33350595457
Aditya Chaturvedi <kalelrjbrownboy@gmail.com>
2026-07-01 03:20:20 +0530

    Midas touch

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app=express();
 const uploadsRoutes=require('./routes/uploads.route');
 const path=require('path');
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/uploads',uploadsRoutes);
